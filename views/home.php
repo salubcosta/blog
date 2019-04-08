63fb28db728b88c0cfddda68fd9827b532ca756f
salubcosta <salubcosta@gmail.com>
2019-04-08 17:36:59 -0300

    incluido algumas divs

--- views/home.php
+++ views/home.php
@@ -1,1 +1,10 @@
-<h1>View Home</h1>
+<div class="leftside">
+	<h1>View Home</h1>	
+</div>
+<div class="rightside">
+	<h2>Autor</h2>
+	<h3>Salumão Barbosa da Costa</h3>
+	<p>
+		Analista de Sistemas, graduado em Sistemas de Informação, Esp. em Engenharia de Software. Adora Programação, Banco de dados e TI em geral! :D
+	</p>
+</div>
