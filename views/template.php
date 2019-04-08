1c6771f20fdd8815e42bedfb41f8c35dade5c31a
salubcosta <salubcosta@gmail.com>
2019-04-08 17:37:24 -0300

    update at template file

--- views/template.php
+++ views/template.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="shortcut icon" href="<?php echo URL;?>/assets/images/s.png" type="image/png">
 	<meta charset="utf-8">
 	<title>Template</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo URL;?>/assets/css/estilo.css">
 </head>
 <body>
-	<?php $this->carregarViewNoTemplate($view, $dados);?>
+	<div class="menu">
+		<div class="menuint">
+			<ul>
+				<a href=""><li><img src="<?php echo URL;?>/assets/images/s.png"></li></a>
+				<a href="<?php echo URL; ?>"><li>Início</li></a>
+				<a href="#"><li>Categorias</li></a>
+				<a href="#"><li>Contato</li></a>
+			</ul>
+			<div class="menuright"><input type="text" name="search"><input type="submit" value="Buscar"> </div>	
+		</div>
+	</div>
+	<div style="clear: both;"></div>
+	<div class="topo">
+		<div class="topoint">
+			<div class="topoleft">
+				<h1>#<a href="<?php echo URL; ?>">Salumao.com.br</a></h1>
+			</div>
+			<div class="toporight">
+				<div class="conteudo">
+					<p>Este blog irá tratar de assuntos relacionados à área de TI</p>
+					<p>Resumos, traduções de artigos, questões entre outros.</p>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="container">
+		<?php $this->carregarViewNoTemplate($view, $dados);?>
+	</div>
 </body>
 </html>
