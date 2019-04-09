d994d2e3e3255ee2a070365cb5ff6fb4799a7e91
salubcosta <salubcosta@gmail.com>
2019-04-09 18:05:36 -0300

    várias pequenas mudanças...

--- views/home.php
+++ views/home.php
@@ -1,14 +1,44 @@
-<div class="leftside">
-	<h1>View Home</h1>	
-</div>
-<div class="rightside">
-	<h4>Salumão Barbosa da Costa</h4>
-	<span class="minihr"></span>
-	<div class="imgautor">
-		<img src="<?php echo URL;?>/assets/images/devimg.png">
+<div class="right">
+	<div class="rightside">
+		<h4>Salumão Barbosa da Costa</h4>
+		<span class="minihr"></span>
+		<div class="imgautor">
+			<img src="<?php echo URL;?>/assets/images/devimg.png">
+		</div>
+		<p>
+			Analista de Sistemas, graduado em Sistemas de Informação, Esp. em Engenharia de Software. Adora Programação, Banco de dados e assuntos relacionados à Tecnologia da Informação!
+		</p>
+		<a href="#">Sobre mim</a>
 	</div>
-	<p>
-		Analista de Sistemas, graduado em Sistemas de Informação, Esp. em Engenharia de Software. Adora Programação, Banco de dados e assuntos relacionados à Tecnologia da Informação!
-	</p>
-	<a href="#">Sobre mim</a>
+	<div class="rightside">
+		<h4>Mais lidos</h4>
+		<p>Como configurar Apache, PHP e PHPMyadmin no linux Mint</p>
+		<p>Como configurar Apache, PHP e PHPMyadmin no linux Mint</p>
+		<p>Como configurar Apache, PHP e PHPMyadmin no linux Mint</p>
+		<p>Como configurar Apache, PHP e PHPMyadmin no linux Mint</p>
+	</div>
 </div>
+<div class="left">
+	<?php for($i=0; $i<5; $i++): ?>
+	<div class="leftside">
+		<div class="postimg">
+			<img src="<?php echo URL; ?>/assets/images/semimg.png">
+		</div>
+		<div class="posttitle">
+			<h3>Como implementar uma estrutura MVC com PHP</h3>	
+		</div>
+		<div class="postconteudo">
+			<p>
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged...
+			</p>	
+		</div>
+		<div class="postbotoes">
+			<ul>
+				<li><a href="#">Leia mais</a></li>
+				<li><a href="#">Compartilhar</a></li>
+			</ul>
+		</div>
+	</div>
+	<?php endfor; ?>
+</div>
+
--- views/template.php
+++ views/template.php
@@ -3,22 +3,24 @@
 <head>
 	<link rel="shortcut icon" href="<?php echo URL;?>/assets/images/s.png" type="image/png">
 	<meta charset="utf-8">
-	<title>Template</title>
+	<title>Salumao.com.br</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo URL;?>/assets/css/estilo.css">
 </head>
 <body>
+	<!--##################### MENU #####################-->
 	<div class="menu">
 		<div class="menuint">
 			<ul>
 				<a href=""><li><img src="<?php echo URL;?>/assets/images/s.png"></li></a>
-				<a href="<?php echo URL; ?>"><li>Início</li></a>
+				<!--<a href="<?php echo URL; ?>"><li>Início</li></a>
 				<a href="#"><li>Categorias</li></a>
-				<a href="#"><li>Contato</li></a>
+				<a href="#"><li>Contato</li></a>-->
 			</ul>
 			<div class="menuright"><input type="text" name="search"><input type="submit" value="Buscar"> </div>	
 		</div>
 	</div>
-	<div style="clear: both;"></div>
+	<!--##################### FIM MENU
+	<div style="clear: both;"></div>-->
 	<div class="topo">
 		<div class="topoint">
 			<div class="topoleft">
@@ -30,11 +32,44 @@
 					<p>Resumos, traduções de artigos, questões entre outros.</p>
 				</div>
 			</div>
+			<div class="space"></div>
 		</div>
-
 	</div>
+	<!--##################### MENU #####################-->
+	<div class="menu">
+		<div class="menuint">
+			<ul>
+				<!--<a href=""><li><img src="<?php echo URL;?>/assets/images/s.png"></li></a>-->
+				<a href="<?php echo URL; ?>"><li>Início</li></a>
+				<a href="#"><li>Categorias</li></a>
+				<a href="#"><li>Contato</li></a>
+			</ul>
+			<!--<div class="menuright"><input type="text" name="search"><input type="submit" value="Buscar"> </div>	-->
+		</div>
+	</div>
+	<!--##################### FIM MENU #####################-->
 	<div class="container">
 		<?php $this->carregarViewNoTemplate($view, $dados);?>
+	</div>
+	<div class="rodape">
+		<div class="rodapeint">
+			<ul>
+				<li><a href="#">Sobre</a></li>
+				<li><a href="#">Categorias</a></li>
+				<li><a href="#">Contato</a></li>
+				<li><strong>Mídias Sociais:</strong></li>
+				<li><a href="#">Instagram</a></li>
+				<li><a href="#">Facebook</a></li>
+				<li><a href="#">Twitter</a></li>
+			</ul>
+			<ul>
+				<li><a href="#">Github</a>|</li>
+				<li><a href="#">Linkedin</a></li>
+			</ul>
+		</div>
+		<footer>
+			<p>© 2019 Salumao.com.br | All rights reserved</p>
+		</footer>
 	</div>
 </body>
 </html>
