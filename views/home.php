d066bc0fdd27c5e4f55829d78fd2703316560a24
salubcosta <salubcosta@gmail.com>
2019-04-09 01:07:37 -0300

    some changing in the file

--- views/home.php
+++ views/home.php
@@ -2,9 +2,13 @@
 	<h1>View Home</h1>	
 </div>
 <div class="rightside">
-	<h2>Autor</h2>
-	<h3>Salumão Barbosa da Costa</h3>
+	<h4>Salumão Barbosa da Costa</h4>
+	<span class="minihr"></span>
+	<div class="imgautor">
+		<img src="<?php echo URL;?>/assets/images/devimg.png">
+	</div>
 	<p>
-		Analista de Sistemas, graduado em Sistemas de Informação, Esp. em Engenharia de Software. Adora Programação, Banco de dados e TI em geral! :D
+		Analista de Sistemas, graduado em Sistemas de Informação, Esp. em Engenharia de Software. Adora Programação, Banco de dados e assuntos relacionados à Tecnologia da Informação!
 	</p>
+	<a href="#">Sobre mim</a>
 </div>
