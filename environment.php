e7d41f001f6d4cbb923df1eca9fde3427dfcac64
salubcosta <salubcosta@gmail.com>
2019-04-16 17:36:32 -0300

    'update'

--- environment.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define('ENV','development'); //AMBIENTE - Desenvolvimento
-
-define('URL','http://127.0.0.1/projetos/blog/'); //URL raiz
-
-define('DIR', dirname(__FILE__)); //Diretório raiz
-?>
