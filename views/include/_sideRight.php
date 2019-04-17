724fdf3c4157c07d5fe445998e929758bdc1d9e2
salubcosta <salubcosta@gmail.com>
2019-04-17 01:15:58 -0300

    facilidades com inclusões...

---
+++ views/include/_sideRight.php
@@ -0,0 +1,24 @@
+<div class="p-3">
+  <h4 class="font-italic">Sobre o projeto</h4>
+  <p class="mb-0 text-justify mb-3">Este blog irá abordar assuntos relacionados à área da tecnologia da informação, programação, banco de dados e outros tópicos como resumos de artigos, posts com questões, mapas mentais, um verdadeiro acervo pessoal para estudo.</p>
+  <a href="#" class="btn btn-secondary">Saiba mais sobre o projeto</a>
+</div>
+<div class="p-3">
+  <h4 class="font-italic">Mais lidos</h4>
+    <ol class="list-unstyled">
+      <li><a href="<?php echo URL.'post'; ?>">Como configurar Github no Windows?</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">Configurar servidor WEB permitir .HTACCESS</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">COBIT 5 - Introdução</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">Python na prática</a></li>
+    </ol>
+    <a href="#" class="btn btn-secondary">Veja mais lidos</a>
+</div>
+<div class="p-3">
+  <h4 class="font-italic">Em outros lugares</h4>
+  <ol class="list-unstyled">
+    <li><a href="https://GitHub.com/salubcosta" target="_blank">GitHub</a></li>
+    <li><a href="https://Twitter.com/salubcosta" target="_blank">Twitter</a></li>
+    <li><a href="https://fb.com/salubcosta" target="_blank">Facebook</a></li>
+  </ol>
+</div>
