20b2d6b4e1b02e2f89722ba2a4f451c60a63537e
salubcosta <salubcosta@gmail.com>
2019-04-17 01:16:20 -0300

    adicionado view post para tratar as postagens

---
+++ views/post.php
@@ -0,0 +1,20 @@
+<div class="row">
+	<div class="col-md-8">
+		<div class="p mt-3">
+			<h4 class="text-dark">Configurar apache, php e phpmyadmin at Linux MINT</h4>
+			<p class="text-muted font-italic mt-3">16 de Abril de 2019 publicado por <a href="#">Salumão</a></p>
+			<div class="text-justify mt-3">
+				<p>Este artigo irá versar sobre como instalar e configurar um servidor apache no linux mint, precisamente falando, o linux mint mate cinammon 19.1. Também vamos config...</p>
+				<?php for ($i=0; $i < 3; $i++):?>
+				<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+				</p>
+			<?php endfor; ?>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<?php include DIR.'/views/include/_sideRight.php'; ?>
+	</div>
+	
+
+</div>
