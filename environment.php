f97e8eb5abd54eb7d3e5d87e95f83844632fdf10
salubcosta <salubcosta@gmail.com>
2019-04-17 01:14:14 -0300

    atualização de variáveis devido mudança de SO

--- environment.php
+++ environment.php
@@ -1,7 +1,7 @@
 <?php 
 define('ENV','development'); //AMBIENTE - Desenvolvimento
 
-define('URL','http://10.98.128.254/cursos/blog/'); //URL raiz
+define('URL','http://localhost/projetos/blog/'); //URL raiz
 
 define('DIR', dirname(__FILE__)); //Diretório raiz
 ?>
