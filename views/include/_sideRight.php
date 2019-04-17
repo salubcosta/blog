0ab76d1a7ddd986f6998665346e630e0d970ae36
salubcosta <salubcosta@gmail.com>
2019-04-17 17:15:31 -0300

    update view

--- views/include/_sideRight.php
+++ views/include/_sideRight.php
@@ -1,10 +1,10 @@
-<div class="p-3">
-  <h4 class="font-italic">Sobre o projeto</h4>
+<!-- <div class="p-3">
+  <h4 class="text-muted">Sobre o projeto</h4>
   <p class="mb-0 text-justify mb-3">Este blog irá abordar assuntos relacionados à área da tecnologia da informação, programação, banco de dados e outros tópicos como resumos de artigos, posts com questões, mapas mentais, um verdadeiro acervo pessoal para estudo.</p>
   <a href="#" class="btn btn-secondary">Saiba mais sobre o projeto</a>
-</div>
+</div> -->
 <div class="p-3">
-  <h4 class="font-italic">Mais lidos</h4>
+  <h4 class="text-muted">Em Destaques</h4>
     <ol class="list-unstyled">
       <li><a href="<?php echo URL.'post'; ?>">Como configurar Github no Windows?</a></li>
       <li><a href="<?php echo URL.'post'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
@@ -15,7 +15,18 @@
     <a href="#" class="btn btn-secondary">Veja mais lidos</a>
 </div>
 <div class="p-3">
-  <h4 class="font-italic">Em outros lugares</h4>
+  <h4 class="text-muted">Novidades</h4>
+    <ol class="list-unstyled">
+      <li><a href="<?php echo URL.'post'; ?>">Como configurar Github no Windows?</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">Python na prática</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">COBIT 5 - Introdução</a></li>
+      <li><a href="<?php echo URL.'post'; ?>">Configurar servidor WEB permitir .HTACCESS</a></li>
+    </ol>
+    <a href="#" class="btn btn-secondary">Últimas atualizações</a>
+</div>
+<div class="p-3">
+  <h4 class="text-muted">Em outros lugares</h4>
   <ol class="list-unstyled">
     <li><a href="https://GitHub.com/salubcosta" target="_blank">GitHub</a></li>
     <li><a href="https://Twitter.com/salubcosta" target="_blank">Twitter</a></li>
