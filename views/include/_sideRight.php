b54db6eaa28884ab2c96c99aef90386f654c16ca
salubcosta <salubcosta@gmail.com>
2019-04-18 17:27:31 -0300

    update view

--- views/include/_sideRight.php
+++ views/include/_sideRight.php
@@ -6,24 +6,36 @@
 <div class="p-3">
   <h4 class="text-muted">Em Destaques</h4>
     <ol class="list-unstyled">
-      <li><a href="<?php echo URL.'post'; ?>">Como configurar Github no Windows?</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">Configurar servidor WEB permitir .HTACCESS</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">COBIT 5 - Introdução</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">Python na prática</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Como configurar Github no Windows?</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Configurar servidor WEB permitir .HTACCESS</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">COBIT 5 - Introdução</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Python na prática</a></li>
     </ol>
     <a href="#" class="btn btn-secondary">Veja mais lidos</a>
 </div>
 <div class="p-3">
   <h4 class="text-muted">Novidades</h4>
     <ol class="list-unstyled">
-      <li><a href="<?php echo URL.'post'; ?>">Como configurar Github no Windows?</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">Python na prática</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">COBIT 5 - Introdução</a></li>
-      <li><a href="<?php echo URL.'post'; ?>">Configurar servidor WEB permitir .HTACCESS</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Como configurar Github no Windows?</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Python na prática</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Gerenciar permissão de usuário MariaDB</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">COBIT 5 - Introdução</a></li>
+      <li><a href="<?php echo URL.'post/ler/1'; ?>">Configurar servidor WEB permitir .HTACCESS</a></li>
     </ol>
     <a href="#" class="btn btn-secondary">Últimas atualizações</a>
+</div>
+<div class="p-3">
+  <h4 class="text-muted">Categorias</h4>
+  <ol class="list-unstyled">
+    <li><a href="#">Banco de Dados <span class="badge badge-primary">4</span> </a></li>
+    <li><a href="#">Programação <span class="badge badge-primary">3</span></a></li>
+    <li><a href="#">Redes de Computadores <span class="badge badge-primary">8</span></a></li>
+    <li><a href="#">Segurança da Informação <span class="badge badge-primary">2</span></a></li>
+    <li><a href="#">Engenharia de Software <span class="badge badge-primary">12</span></a></li>
+    <li><a href="#">Gestão de TI <span class="badge badge-primary">5</span></a></li>
+    <li><a href="#">Computação Forense <span class="badge badge-primary">5</span></a></li>
+  </ol>
 </div>
 <div class="p-3">
   <h4 class="text-muted">Em outros lugares</h4>
