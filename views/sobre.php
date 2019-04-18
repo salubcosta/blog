9f799095f2bb0b9ff541622cbe2fd08e603d78cb
salubcosta <salubcosta@gmail.com>
2019-04-18 17:30:42 -0300

    adicionado view sobre

---
+++ views/sobre.php
@@ -0,0 +1,20 @@
+<div class="row">
+	<div class="col-md-8">
+		<div class="p text-muted mt-3 text-justify">
+			<h4 class="mb-3">Sobre este projeto</h4>
+			<p class="mb-0 mb-3">Este blog irá abordar assuntos relacionados à área da tecnologia da informação, programação, banco de dados e outros tópicos como resumos de artigos, posts com questões, mapas mentais, um verdadeiro acervo pessoal para estudo.</p>
+	  		<p>
+	  			Todos os artigos que serão produzidos deverá conter no final a referência ou mesmo a citação. Caso encontre alguma falha, seja de português ou 
+	  			de conceito, favor entre em contato. Embora este artigo tenha neste primeiro momento apenas um autor, vocês, que estão lendo neste blog, poderão
+	  			, sem pestanejar, colaborar com o que está sendo produzindo. Além disso, estou sempre aberto à sugestões de assuntos para abordar aqui.
+	  		</p>
+	  		<p>Sem mais, agradeço desde já sua visita!</p>
+		</div>
+		<div class="p">
+			<p><a href="<?php echo URL.'autor';?>" class="btn btn-info">Saiba sobre mais sobre o Autor desde blog clicando aqui!</a></p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<?php include DIR.'/views/include/_sideRight.php'; ?>
+	</div>
+</div>
