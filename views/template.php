4b7fb6c1c65c95c83c7081fa0515788865ee5827
salubcosta <salubcosta@gmail.com>
2019-04-23 17:10:35 -0300

    update

--- views/template.php
+++ views/template.php
@@ -4,10 +4,7 @@
 	<link rel="shortcut icon" href="<?php echo URL;?>assets/images/s.png" type="image/png">
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  
-  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo URL;?>assets/css/bootstrap.min.css"> 
+ 	<link rel="stylesheet" type="text/css" href="<?php echo URL;?>assets/css/bootstrap.min.css"> 
 	<title>Salumao.com.br</title>
 </head>
 <body>
@@ -79,8 +76,6 @@
     <?php $this->carregarViewNoTemplate($view, $dados);?>
   </div>
   <!-- /.conteúdo -->
- 	
-<!-- style="background-color: #E9ECEF;"  -->
   <footer class="pt-5 pb-5" >
     <div class="container">
       <div class="row pt-5 border-top">
