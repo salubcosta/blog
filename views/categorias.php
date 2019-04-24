6827c6342e584aa9771df91523cd87ca36956278
salubcosta <salubcosta@gmail.com>
2019-04-24 11:13:54 -0300

    correção

--- views/categorias.php
+++ views/categorias.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-md-8">
-		<h4 class="pb-2 pt-3 mb-4 border-bottom text-muted text-uppercase">Novidades <small class="font-italic text-capitalize">#<?php echo $categoria; ?>!</small></h4>
+		<h4 class="pb-2 pt-3 mb-4 border-bottom text-muted text-uppercase">Categorias <small class="font-italic text-capitalize">#<?php echo $categoria; ?>!</small></h4>
 	</div>
 	<div class="col-md-4">
 		<?php include DIR.'/views/include/_sideRight.php'; ?>
