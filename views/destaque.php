659221fa899bb82c740b44004f69cc5c2ab8784c
salubcosta <salubcosta@gmail.com>
2019-04-24 10:59:46 -0300

    adicionado view destaque

---
+++ views/destaque.php
@@ -0,0 +1,9 @@
+<div class="row">
+	<div class="col-md-8">
+		<h4 class="pb-2 pt-3 mb-4 border-bottom text-muted text-uppercase">Destaques <small class="font-italic text-capitalize">#Maislidos!</small></h4>
+	</div>
+	<div class="col-md-4">
+		<?php include DIR.'/views/include/_sideRight.php'; ?>
+	</div>
+
+</div>
